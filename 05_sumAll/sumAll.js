8133b76dbe01e2a523656a0da5e9adb7809e8210
Harneet Sahi <134643674+harneetsahi@users.noreply.github.com>
2024-08-09 21:06:25 +0530

    Update sumAll.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function (firstNum, lastNum) {
+  let sum = 0;
 
+  if (
+    !Number.isInteger(firstNum) ||
+    !Number.isInteger(lastNum) ||
+    firstNum < 0 ||
+    lastNum < 0
+  ) {
+    return "ERROR";
+  }
+
+  if (firstNum > lastNum) {
+    const tempNum = firstNum;
+    firstNum = lastNum;
+    lastNum = tempNum;
+  }
+
+  for (let i = firstNum; i <= lastNum; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
