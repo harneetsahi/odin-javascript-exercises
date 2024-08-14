df7f72509ecc64ce2e6148c1b86422be21a78444
Harneet Sahi <134643674+harneetsahi@users.noreply.github.com>
2024-08-14 23:17:42 +0530

    Updated with the answer

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  const acceptableChars = "abcdefghijklmnopqrstuvwxyz1234567890";
+  const newStr = str
+    .toLowerCase()
+    .split("")
+    .filter((char) => acceptableChars.includes(char))
+    .join("");
 
+  const reversedStr = newStr.split("").reverse().join("");
+
+  return newStr === reversedStr;
 };
 
 // Do not edit below this line
