25ad7e2b1b22b6775fb6347a413bdbe49fb38637
Harneet Sahi <134643674+harneetsahi@users.noreply.github.com>
2024-08-15 13:15:38 +0530

    Update w the answer

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function (index) {
+  if (typeof index !== "number") {
+    index = parseInt(index);
+  }
 
+  if (index < 0) return "OOPS";
+  if (index == 0) return 0;
+
+  const fibSequence = [0, 1];
+
+  for (let i = 2; i <= index; i++) {
+    fibSequence[i] = fibSequence[i - 1] + fibSequence[i - 2];
+  }
+
+  return fibSequence[index];
 };
 
 // Do not edit below this line
